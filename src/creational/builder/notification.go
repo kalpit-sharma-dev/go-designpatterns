e900ea3d177c69224e6a77cdc6659679ae1e9b1e
Kalpit <kalpit.cool2006@gmail.com>
2022-01-19 18:06:41 +0530

    adding singleton

--- src/creational/builder/notification.go
+++ src/creational/builder/notification.go
@@ -1,5 +1,11 @@
 package builder
 
-type Notification struct{
-	
+type Notification struct {
+	title    string
+	subtitle string
+	message  string
+	image    string
+	icon     string
+	priority int
+	notType  string
 }
